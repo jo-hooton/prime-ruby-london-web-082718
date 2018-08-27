78262916430a61654895dbcadc0eee57213f4e9a
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-27 14:44:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(x)
  case
   when  x == 2 || x != 1 || x != 0 || x != -1
     return true
-  when x == -1
+  when x == 0 || x == -1
     return false
   when x.even? == true
     return false
